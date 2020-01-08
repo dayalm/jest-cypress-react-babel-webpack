ac248e21303337451c53a8f74b560cc10206e99f
Dayal Murukutla <dayalm@gmail.com>
2020-01-08 12:46:53 -0500

    add jest lint support, husky and lint staged

--- test/jest-common.js
+++ test/jest-common.js
@@ -12,4 +12,9 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./style-mock.js'),
   },
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
